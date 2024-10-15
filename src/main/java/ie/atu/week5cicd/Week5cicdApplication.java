47e95b3d8ea58a9510a18096adc2557442431c83
G00425238@atu.ie <g00425238@atu,ie>
2024-10-15 10:04:49 +0100

    Created new project

---
+++ src/main/java/ie/atu/week5cicd/Week5cicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week5cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5cicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5cicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week5cicd/Week5cicdApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week5cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5cicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
